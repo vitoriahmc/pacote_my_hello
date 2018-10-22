1c3415bc2aedece5414dd03b1af7811edceea69b
Vitoria Camilo <vitoriahmc@al.insper.edu.br>
2018-10-22 16:32:15 -0300

    versao inicial

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import my_hello
 
 my_hello.world()
--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='my_hello_vitoria', version='0.1', packages=['my_hello'])
+setup(name='my_hello_vitoria', version='0.1', packages=['my_hello'], author='Vitoria Helena', classifiers=[
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ], install_requires=['pytest'], scripts=['scripts/hello.py'])
